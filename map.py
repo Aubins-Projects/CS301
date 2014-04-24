da67ef45c3131cb0d23bd0decc58096cedaf48b4
ryan-johnson2 <ryanakapeter@gmail.com>
2014-04-24 17:14:02 -0400

    create map.py
    Adds map.py which displays a map when called.

---
+++ map.py
@@ -0,0 +1,42 @@
+import pygame, sys
+from pygame.locals import *
+
+def display(level):
+	screen_h = 1012
+	screen_w = 724
+
+	background_img = "full_map2.png"
+	block_img = "block.png"
+	blocks = {}
+
+	pygame.init()
+
+	screen = pygame.display.set_mode((screen_w, screen_h), 0, 32)
+	pygame.display.set_caption("Test")
+	background = pygame.image.load(background_img).convert()
+	for room in level.rooms:
+		blocks[room.name] = pygame.image.load(block_img).convert()
+
+	try:
+		while True:
+			
+			for event in pygame.event.get():
+				
+				if event.type == QUIT:
+					pygame.quit()
+					raise Break()
+					
+				elif event.type == KEYDOWN:
+					if event.key == K_q:
+						pygame.quit()
+						raise Break()
+							
+			screen.blit(background, (0, 0))
+			for room in level.rooms:
+				if not room.visited:
+					screen.blit(blocks[room.name], (room.map_x, room.map_y))
+				
+				
+			pygame.display.update()
+	except:
+		pass
