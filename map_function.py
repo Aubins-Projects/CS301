c3f7251381f0cdb882b356bcb2f4b4d7f89a1749
ryan-johnson2 <ryanakapeter@gmail.com>
2014-05-10 12:17:20 -0400

    Added Current Room
    Added a current room indicator to the functionality of the map

--- map_function.py
+++ map_function.py
@@ -1,12 +1,13 @@
 import pygame, sys
 from pygame.locals import *
 
-def display(level):
+def display(level, room):
   screen_h = 1012
   screen_w = 724
 
   background_img = "full_map2.png"
   block_img = "block.png"
+  curr_room_img = "curr_room.png"
   blocks = {}
 
   pygame.init()
@@ -14,6 +15,8 @@ def display(level):
   screen = pygame.display.set_mode((screen_w, screen_h), 0, 32)
   pygame.display.set_caption("MAP")
   background = pygame.image.load(background_img).convert()
+  curr_room = pygame.image.load(curr_room_img).convert()
+	curr_room.set_colorkey((255,255,255), RLEACCEL)
   for room in level.rooms:
     blocks[room.name] = pygame.image.load(block_img).convert()
 
@@ -40,7 +43,8 @@ def display(level):
         if not room.visited:
           screen.blit(blocks[room.name], (room.map_x, room.map_y))
         
-        
+      screen.blit(curr_room, (room.map_x - 1 , room.map_y - 1))
+      
       pygame.display.update()
   except:
     pygame.quit()
